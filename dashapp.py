885a782eb65f56daf80dd63f326600a27b293b5f
Juha Yrjölä <juha.yrjola@iki.fi>
2021-02-17 14:01:25 +0200

    Implement uuid selector

--- dashapp.py
+++ dashapp.py
@@ -1,39 +1,74 @@
 import os
 import dash
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
+from sqlalchemy import create_engine
+
 
 from dotenv import load_dotenv
 load_dotenv()
 
+TABLE_NAME = 'trips_location'
+DEFAULT_ZOOM = 12
+
+eng = create_engine(os.getenv('DATABASE_URL'))
+
+
+def read_uuids():
+    with eng.connect() as conn:
+        print('Reading uids')
+        res = conn.execute(f'SELECT uuid, count(id) AS count FROM {TABLE_NAME} WHERE aconf IS NOT NULL GROUP BY uuid ORDER BY count DESC LIMIT 100');
+        rows = res.fetchall()
+        uuids = [str(row[0]) for row in rows]
+    return uuids
+
+
+try:
+    uuids = [x.strip() for x in open('uuids.txt', 'r').readlines()]
+except FileNotFoundError:
+    open('uuids.txt', 'w').write('\n'.join(read_uuids()))
+
+
+def read_locations(uid):
+    print('Selected UID %s. Reading dataframe.' % uid)
+    with eng.connect() as conn:
+        df = pd.read_sql_query(f"""
+            SELECT
+                time,
+                ST_X(ST_Transform(loc, 3067)) AS x,
+                ST_Y(ST_Transform(loc, 3067)) AS y,
+                acc,
+                atype,
+                aconf,
+                speed,
+                heading
+            FROM {TABLE_NAME} WHERE uuid = '%s'::uuid ORDER BY time
+        """ % uid, conn)
+
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x, df.y, crs='epsg:3067'))
+    gdf = gdf.to_crs('EPSG:4326')
+    gdf['lat'] = gdf.geometry.y
+    gdf['lon'] = gdf.geometry.x
+    gdf['size'] = 1
+
+    gdf['datetime'] = pd.to_datetime(gdf.time)
+    gdf['speed'] *= 3.6
+    print(gdf)
+
+    return gdf
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 px.set_mapbox_access_token(os.getenv('MAPBOX_ACCESS_TOKEN'))
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-DEFAULT_ZOOM = 12
-
-df = pd.read_pickle('trajstuff.pickle')
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x2, df.y2, crs='epsg:3857'))
-gdf = gdf.to_crs('EPSG:4326')
-gdf['lat'] = gdf.geometry.y
-gdf['lon'] = gdf.geometry.x
-gdf['size'] = 1
-gdf['datetime'] = pd.to_datetime(gdf.time.astype(int), unit='s')
-#gdf.iloc[100]['highlighted'] = 1
-
-map_fig = px.scatter_mapbox(
-    gdf, lat="lat", lon="lon", color='atype', zoom=DEFAULT_ZOOM, height=400, hover_data=['atype', 'speed', 'acc'], size='size', size_max=5
-)
-map_fig.update_layout(mapbox_style="open-street-map", margin={'l': 0, 'b': 0, 'r': 0, 't': 10})
-
-time_fig = px.scatter(gdf, x='datetime', y='speed', color='atype', hover_data=['acc'])
-
+map_fig = None
+gdf = None
 
 @app.callback(
     Output('map-graph', 'figure'),
@@ -79,29 +114,45 @@ def display_hover_data(hoverData, clickData):
 
     return map_fig
 
-    m_size = map_fig['data'][0]['marker']['size']
-    m_idxs = gdf.index[gdf.time.isin(x_vals)]
-    print(map_fig)
-    for i in m_idxs:
-        m_size[i] = 2
-    """
-    #map_fig.loc[map_fig.time.isin(x_vals), 'size'] = 3
-    #map_fig.loc[~map_fig.time.isin(x_vals), 'size'] = 1
-    """
+@app.callback(
+    Output('graph-container', 'children'),
+    [Input('uuid-selector', 'value')]
+)
+def handle_uuid_selection(new_uid):
+    global map_fig, gdf
 
-    return map_fig
+    if not new_uid:
+        return None
+    gdf = read_locations(new_uid)
 
-
-app.layout = html.Div(children=[
-    dcc.Graph(
-        id='map-graph',
-        figure=map_fig
-    ),
-    dcc.Graph(
-        id='time-graph',
-        figure=time_fig
+    map_fig = px.scatter_mapbox(
+        gdf, lat="lat", lon="lon", color='atype', zoom=DEFAULT_ZOOM,
+        height=400, hover_data=['atype', 'speed', 'acc'], size='size', size_max=5
     )
-])
+    map_fig.update_layout(mapbox_style="open-street-map", margin={'l': 0, 'b': 0, 'r': 0, 't': 10})
+
+    time_fig = px.scatter(gdf, x='datetime', y='speed', color='atype', hover_data=['acc'])
+
+    return [
+        dbc.Col(dcc.Graph(id='map-graph', figure=map_fig), md=6),
+        dbc.Col(dcc.Graph(id='time-graph', figure=time_fig), md=6),
+    ]
+
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Select(
+                id="uuid-selector",
+                options=[{'label': x, 'value': x} for x in uuids]
+            )
+        ], md=4)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='map-graph'), md=6),
+        dbc.Col(dcc.Graph(id='time-graph'), md=6),
+    ], id='graph-container'),
+], fluid=True)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
