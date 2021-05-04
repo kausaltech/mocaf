8762cbdea35448f04fcb4268c0229c9576f5a170
Bernhard Bliem <bernhard@bliem.xyz>
2021-05-05 01:18:28 +0300

    Expand HTML of rich text fields to fix images

--- pages/schema.py
+++ pages/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from wagtail.core.rich_text import expand_db_html
 
 from . import models
 
@@ -9,15 +10,20 @@ class BlogPost(DjangoObjectType):
         model = models.BlogPost
         only_fields = ['id', 'title', 'tagline', 'body', 'first_published_at']
 
+    body = graphene.String()
+
+    def resolve_body(root, info):
+        return expand_db_html(root.body)
+
 
 class Query(graphene.ObjectType):
     blog_post = graphene.Field(BlogPost, id=graphene.Int(required=True))
     blog_posts = graphene.List(BlogPost)
 
-    def resolve_blog_post(self, info, id, **kwargs):
+    def resolve_blog_post(root, info, id, **kwargs):
         return models.BlogPost.objects.get(id=id)
 
-    def resolve_blog_posts(self, info, **kwargs):
+    def resolve_blog_posts(root, info, **kwargs):
         return (models.BlogPost.objects
                 .live()
                 .public()
