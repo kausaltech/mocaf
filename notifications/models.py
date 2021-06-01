b8e99122dbb2a690c431bf6d18f81dbded964fa6
Bernhard Bliem <bernhard@bliem.xyz>
2021-06-01 19:22:23 +0300

    Render body of NotificationTemplate instances with Jinja2

--- notifications/models.py
+++ notifications/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from jinja2 import StrictUndefined, Template
 from modeltrans.fields import TranslationField
 from wagtail.admin.edit_handlers import FieldPanel
 
@@ -9,6 +10,12 @@ class EventTypeChoices(models.TextChoices):
     MONTHLY_SUMMARY = 'monthly_summary', _("Monthly summary")
     WELCOME_MESSAGE = 'welcome_message', _("Welcome message")
     NO_RECENT_TRIPS = 'no_recent_trips', _("No recent trips")
+
+
+class BodyPanel(FieldPanel):
+    def on_form_bound(self):
+        super().on_form_bound()
+        self.help_text = _("Notification body; variables like x can be substituted using the syntax {{ x }}")
 
 
 class NotificationTemplate(models.Model):
@@ -23,8 +30,16 @@ class NotificationTemplate(models.Model):
     ] + [
         FieldPanel(f'title_{language}') for language, _ in settings.LANGUAGES
     ] + [
-        FieldPanel(f'body_{language}') for language, _ in settings.LANGUAGES
+        BodyPanel(f'body_{language}') for language, _ in settings.LANGUAGES
     ]
 
     def __str__(self):
         return self.title
+
+    def render_body(self, **kwargs):
+        """Render the body in the active language as a Jinja2 template.
+
+        kwargs are passed as context when rendering the template.
+        """
+        template = Template(self.body_i18n, undefined=StrictUndefined)
+        return template.render(**kwargs)
