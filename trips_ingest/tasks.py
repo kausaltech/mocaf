1c29fd346b3225868ff2a87379837913e23e6156
Juha Yrjölä <juha.yrjola@iki.fi>
2021-06-03 17:01:17 +0300

    Add a task to perform trips_ingest cleanup

--- trips_ingest/tasks.py
+++ trips_ingest/tasks.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
 import logging
 from celery import shared_task
+from django.utils import timezone
 
 from .processor import EventProcessor
+from .models import Location
 
 
 logger = logging.getLogger(__name__)
@@ -12,3 +15,11 @@ processor = EventProcessor()
 def ingest_events():
     logger.info('Processing events')
     processor.process_events()
+
+
+@shared_task
+def cleanup():
+    logger.info('Cleaning up')
+    yesterday = timezone.now() - timedelta(days=1)
+    # Delete locations that user has marked for deletion
+    Location.objects.filter(deleted_at__isnull=False).filter(deleted_at__lte=yesterday).delete()
