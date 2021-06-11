954b9e273bac63233425f05e8f638415b7260a11
Bernhard Bliem <bernhard@bliem.xyz>
2021-06-11 15:39:20 +0300

    Use inspect view instead of edit view for feedback

--- feedback/wagtail_hooks.py
+++ feedback/wagtail_hooks.py
@@ -4,7 +4,19 @@ from .models import DeviceFeedback
 
 
 class DeviceFeedbackPermissionHelper(PermissionHelper):
+    def user_can_list(self, user):
+        return True
+
     def user_can_create(self, user):
+        return False
+
+    def user_can_inspect_obj(self, user, obj):
+        return True
+
+    def user_can_delete_obj(self, user, obj):
+        return True
+
+    def user_can_edit_obj(self, user, obj):
         return False
 
 
@@ -14,3 +26,4 @@ class DeviceFeedbackAdmin(ModelAdmin):
     menu_icon = 'mail'
     permission_helper_class = DeviceFeedbackPermissionHelper
     list_display = ['created_at', 'device_brand', 'comment']
+    inspect_view_enabled = True
