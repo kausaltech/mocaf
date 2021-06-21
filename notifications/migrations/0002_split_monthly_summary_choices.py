c9ccb566f28ff431250320d5691b407c5327b16e
Bernhard Bliem <bernhard@bliem.xyz>
2021-06-21 08:41:22 +0300

    Make migration reversible

--- notifications/migrations/0002_split_monthly_summary_choices.py
+++ notifications/migrations/0002_split_monthly_summary_choices.py
@@ -8,6 +8,11 @@ def migrate_notification_templates(apps, schema_editor):
     NotificationTemplate.objects.filter(event_type='monthly_summary').update(event_type='monthly_summary_bronze')
 
 
+def migrate_notification_templates_reverse(apps, schema_editor):
+    NotificationTemplate = apps.get_model('notifications', 'NotificationTemplate')
+    NotificationTemplate.objects.filter(event_type='monthly_summary_bronze').update(event_type='monthly_summary')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -20,5 +25,5 @@ class Migration(migrations.Migration):
             name='event_type',
             field=models.CharField(choices=[('monthly_summary_gold', 'Monthly summary (gold-level budget)'), ('monthly_summary_silver', 'Monthly summary (silver-level budget)'), ('monthly_summary_bronze', 'Monthly summary (bronze-level budget or worse)'), ('welcome_message', 'Welcome message'), ('no_recent_trips', 'No recent trips')], max_length=22),
         ),
-        migrations.RunPython(migrate_notification_templates),
+        migrations.RunPython(migrate_notification_templates, migrate_notification_templates_reverse),
     ]
