f06ea1fe3dabf97989215fc157c041101c1d03f9
Timo Tuominen <tituomin@gmail.com>
2021-12-14 17:40:44 +0200

    Add reducer module

---
+++ analytics/client/src/userChoiceReducer.js
@@ -0,0 +1,13 @@
+export const initialUserChoiceState = {
+  weekSubset: 'workday',
+  transportMode: 'car',
+  areaType: 5,
+  analyticsQuantity: 'lengths'
+}
+
+export function userChoiceReducer(state, action) {
+  if (action.type !== 'set' || action.payload === undefined || !action.key) {
+    return state;
+  }
+  return Object.assign({}, state, {[action.key]: action.payload});
+}
