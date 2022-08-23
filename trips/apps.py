3de3bfb30eff102c8844f6392454ef9b3e3ec0e1
Juha Yrjölä <juha.yrjola@iki.fi>
2022-08-23 10:28:07 +0300

    Do not set up the Prometheus gauge when running under pytest

--- trips/apps.py
+++ trips/apps.py
@@ -21,8 +21,9 @@ class TripsConfig(AppConfig):
         # https://stackoverflow.com/questions/65072296/django-execute-code-only-for-manage-py-runserver-not-for-migrate-help-e
         is_manage_py = any(arg.endswith("manage.py") for arg in sys.argv)
         is_runserver = any(arg == "runserver" for arg in sys.argv)
+        is_pytest = 'pytest' in sys.modules
 
-        if (is_manage_py and is_runserver) or (not is_manage_py):
+        if not is_pytest and (is_manage_py and is_runserver) or (not is_manage_py):
             from trips.models import TransportMode
             gauge = Gauge('trips_length_meters_total', "Total meters by mode of transportation", ['mode'])
             for mode in TransportMode.objects.all():
